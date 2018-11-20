2218bc566aefa6524ef7178c012b22390f3d9ac6
Saransh Sharma <saransh1337@gmail.com>
2018-11-20 17:27:58 +0530

    add simple test

---
+++ app/src/androidTest/java/com/mycompany/greeter/MainActivityTests.java
@@ -0,0 +1,33 @@
+package com.mycompany.greeter;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTests {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule
+            = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testGreet() {
+        onView(withId(R.id.greetEditText))
+                .perform(typeText("saransh1337"), closeSoftKeyboard());
+
+        onView(withText("Greet")).perform(click());
+
+        onView(withId(R.id.messageTextView))
+                .check(matches(withText("Hello, saransh1337!")));
+    }
+}
